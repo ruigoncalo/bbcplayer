308f0397a319ade434030f8e19681209aa90cbb5
Rui Gonçalo <ruimiguelgoncalo@gmail.com>
2014-03-21 13:36:35 +0000

    json parser initialized

---
+++ BBCPlayer/src/com/rmgoncalo/bbcplayer/JsonParser.java
@@ -0,0 +1,31 @@
+/*
+ * Class used to parse json data
+ */
+
+/*
+ *
+ * Example: The Last Station
+ * Link: http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=qrbt5gajqa75qutk6mftdjvk&q=thelaststation
+ * 
+ * {
+ * 	"total":0,
+ * 	"movies":[],
+ * 	"links":
+ * 		{
+ * 			"self":"http://api.rottentomatoes.com/api/public/v1.0/movies.json?q=thelaststation&page_limit=30&page=1"},
+ * 			"link_template":"http://api.rottentomatoes.com/api/public/v1.0/movies.json?q={search-term}&page_limit={results-per-page}&page={page-number}"
+ * 		}
+ * }
+ * 
+ * No score element found!!
+ * 
+ */
+
+
+package com.rmgoncalo.bbcplayer;
+
+public class JsonParser {
+	
+	//TODO: implement Json parser
+		
+}
