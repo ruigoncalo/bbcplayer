a7ed7237d6f494921ec6a09a74c2891d56d54512
Rui Gonçalo <ruimiguelgoncalo@gmail.com>
2014-04-08 14:11:56 +0100

    added imageview method

--- BBCPlayer/src/com/rmgoncalo/bbcplayer/EntryArrayAdapter.java
+++ BBCPlayer/src/com/rmgoncalo/bbcplayer/EntryArrayAdapter.java
@@ -12,35 +12,42 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class EntryArrayAdapter extends ArrayAdapter<Entry>{
-	
-	//private static final String logtag = "ArrayAdapter";
+public class EntryArrayAdapter extends ArrayAdapter<Entry> {
+
+	// private static final String logtag = "ArrayAdapter";
 	private final Context context;
 	private final List<Entry> values;
-		
-	public EntryArrayAdapter(Context context, List<Entry> values){
+
+	private ImageLoader imgLoader;
+
+	public EntryArrayAdapter(Context context, List<Entry> values) {
 		super(context, R.layout.entry, values);
 		this.context = context;
 		this.values = values;
+		imgLoader = new ImageLoader(context);
 	}
-			
+
 	// Use the entry layout to each row
 	// Use title and id strings of each Entry object
 	public View getView(int position, View convertView, ViewGroup parent) {
 		LayoutInflater inflater = (LayoutInflater) context
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			
+
 		View rowView = inflater.inflate(R.layout.entry, parent, false);
-			
+
 		TextView titleView = (TextView) rowView.findViewById(R.id.title);
 		TextView idView = (TextView) rowView.findViewById(R.id.id);
-			
-		//ImageView imageView = (ImageView) rowView.findViewById(R.id.thumb);		
+
+		ImageView imageView = (ImageView) rowView.findViewById(R.id.thumb);
+		imgLoader.DisplayImage(values.get(position).getThumb(),
+				R.drawable.ic_launcher, imageView);
+
 		titleView.setText(values.get(position).getTitle());
 		idView.setText(values.get(position).getId());
-			
+
 		return rowView;
 	}
 }
